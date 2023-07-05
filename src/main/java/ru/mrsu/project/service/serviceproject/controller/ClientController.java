069c96c9c2b7bdcfa763764d4713df9a870048b6
LaveeshFarm <slavkin18@yandex.ru>
2023-07-05 11:49:57 +0300

    Создал сервис для проекта clients

---
+++ src/main/java/ru/mrsu/project/service/serviceproject/controller/ClientController.java
@@ -0,0 +1,20 @@
+package ru.mrsu.project.service.serviceproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class ClientController {
+
+    @GetMapping("/client")
+    HashMap<String, Object> getClients() {
+        HashMap<String, Object> hm = new HashMap<>();
+        hm.put("id", 1);
+        hm.put("name", "Tom");
+        hm.put("personalNumber", "AAA111");
+        hm.put("addressId", 1);
+        return hm;
+    }
+}
---
+++ src/test/java/ru/mrsu/project/service/serviceproject/ServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mrsu.project.service.serviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
